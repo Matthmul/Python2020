30904c31b6617faee6acedd15e7dbd15eac52f33
Matthmul <73168857+Matthmul@users.noreply.github.com>
2020-10-28 20:35:53 +0100

    Zestaw 3.1

--- Zestaw 3/Zadanie 3.10.py
+++ Zestaw 3/Zadanie 3.10.py
@@ -1,4 +1,4 @@
-slownik = {
+slownik1 = {
     'I': 1,
     'V': 5,
     'X': 10,
@@ -7,22 +7,46 @@ slownik = {
     'D': 500,
     'M': 1000
 }
+slownik2 = dict(I=1,
+                V=5,
+                X=10,
+                L=50,
+                C=100,
+                D=500,
+                M=1000)
+
+listofroman = ['I', 'V', 'X', 'L', 'C', 'D', 'M']
+listofint = [1, 5, 10, 50, 100, 500, 1000]
+
+slownik3 = dict(zip(listofroman, listofint))
 
 
-def roman2int(word):
+def roman2int(word, dict):
     number = 0
     for i in range(len(word)):
-        if i == 0 or slownik[word[-i]] <= slownik[word[-i - 1]]:
-            number += slownik[word[-i - 1]]
+        if i == 0 or dict[word[-i]] <= dict[word[-i - 1]]:
+            number += dict[word[-i - 1]]
         else:
-            number -= slownik[word[-i - 1]]
+            number -= dict[word[-i - 1]]
     return number
 
 
 if __name__ == "__main__":
-    assert 9 == roman2int("IX")
-    assert 2020 == roman2int("MMXX")
-    assert 999 == roman2int("CMXCIX")
-    assert 1000 == roman2int("M")
-    assert 444 == roman2int("CDXLIV")
-    assert 1234 == roman2int("MCCXXXIV")
+    assert 9 == roman2int("IX", slownik1)
+    assert 2020 == roman2int("MMXX", slownik1)
+    assert 999 == roman2int("CMXCIX", slownik1)
+    assert 1000 == roman2int("M", slownik1)
+    assert 444 == roman2int("CDXLIV", slownik1)
+    assert 1234 == roman2int("MCCXXXIV", slownik1)
+    assert 9 == roman2int("IX", slownik2)
+    assert 2020 == roman2int("MMXX", slownik2)
+    assert 999 == roman2int("CMXCIX", slownik2)
+    assert 1000 == roman2int("M", slownik2)
+    assert 444 == roman2int("CDXLIV", slownik2)
+    assert 1234 == roman2int("MCCXXXIV", slownik2)
+    assert 9 == roman2int("IX", slownik3)
+    assert 2020 == roman2int("MMXX", slownik3)
+    assert 999 == roman2int("CMXCIX", slownik3)
+    assert 1000 == roman2int("M", slownik3)
+    assert 444 == roman2int("CDXLIV", slownik3)
+    assert 1234 == roman2int("MCCXXXIV", slownik3)
